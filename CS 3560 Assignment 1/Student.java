aaa95b73a358dc5778c8068e90a8973a4e8718e7
Ivan <revillaivan97@gmail.com>
2022-09-27 18:30:40 -0700

    moved folder

--- CS 3560 Assignment 1/Student.java
+++ CS 3560 Assignment 1/Student.java
@@ -31,11 +31,14 @@ public class Student {
     }// end DEFAULT METHODS
 
     /**
-     * This method creates a random answer choice letter depending if it SC or MC
-     * and set the answer to this.studentAnswer
+     * This method has a parameter int question type and int numberOfChoices. If the
+     * queation type is single
+     * choice it will run the ACGenerator method. If it is multiple choice then it
+     * will run the MCGenerator method. Both methods create the student answer and
+     * set it to this.studentAnswer
      * 
-     * @param questionType
-     * @param numberOfChoices
+     * @param questionType    int questionType 0 = SC, 1 = MC
+     * @param numberOfChoices int the number of choices available to choose from
      */
     public void randomSetStudentAnswer(int questionType, int numberOfChoices) {
         if (questionType == 0) {
@@ -47,6 +50,7 @@ public class Student {
 
     /**
      * This method creates the student ID randomly picked from a default set String
+     * called
      * notSoRandom
      * 
      * @param lengthOfID integer that defines the length of the String to be
@@ -66,7 +70,9 @@ public class Student {
     }// end IDGenerator
 
     /**
-     * This method randomly picks a letter from an integer parameter
+     * This method randomly picks one letter from a range defined by the
+     * numberOfChoices. If numberOfChoices is 2 it will pick a letter between A and
+     * B. If it is 6 it will pick one letter between A and F.
      * 
      * @param numberOfChoices integer that depends on the number of choices from the
      *                        quesion
@@ -85,7 +91,9 @@ public class Student {
     /**
      * This method randomly generates a String for multiple choice questions. If
      * there are four options,
-     * it can choose, of the four options, different ones at different amounts.
+     * it can choose, of the four options, different letter. If there are 2
+     * numberOfChoices, it will pick A or AB or BA. If there are 3, it will pick A
+     * or AC or BC or CBA etc..
      * 
      * @param numberOfChoices integer defining the limit of choices
      * @return a String of all options that were chosen
